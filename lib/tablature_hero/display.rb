d32fea08a80ced4d25cfdbcc2d8db63fa53746ac
Fred <pf.robert974@gmail.com>
2023-12-16 13:55:48 +1100

    introduce data structure to represent song sections

--- lib/tablature_hero/display.rb
+++ lib/tablature_hero/display.rb
@@ -5,34 +5,42 @@ require_relative "window"
 
 module TablatureHero
   module Display
-    def self.start(time_signature, tempo)      
-      beat_duration = 60.0 / tempo.to_i
-      
+    def self.start(song)      
       window = TablatureHero::Window.new
 
-      current_bar = time_signature.clone
-      loop do
-        window.reset
-        6.times do
-          window.print "|" if current_bar == time_signature
-          current_bar.each { |_note| window.print "--" }
-          10.times do
-            window.print "|"
-            time_signature.each { |_note| window.print "--" }
+      song.each do |section|
+        beat_duration = 60.0 / section[:tempo].to_i
+        time_signature = TablatureHero::TIME_SIGNATURES[section[:time_signature]]        
+        current_bar = time_signature.clone
+        bars_remaining = section[:bars].count
+
+        loop do
+          window.reset
+          6.times do
+            window.print "|" if current_bar == time_signature
+            current_bar.each { |_note| window.print "--" }
+            10.times do
+              window.print "|"
+              time_signature.each { |_note| window.print "--" }
+            end
+            window.newline
           end
-          window.newline
+          window.refresh
+        
+          note = current_bar.shift
+          Thread.new { TablatureHero::Player.play("metro_#{note}") }
+        
+          case window.get_char
+          when "q"
+            exit
+          end
+          sleep beat_duration
+          if current_bar.empty?
+            bars_remaining -= 1
+            break if bars_remaining == 0
+            current_bar = time_signature.clone
+          end
         end
-        window.refresh
-      
-        note = current_bar.shift
-        Thread.new { TablatureHero::Player.play("metro_#{note}") }
-      
-        case window.get_char
-        when "q"
-          break
-        end
-        sleep beat_duration
-        current_bar = time_signature.clone if current_bar.empty?
       end
     end
   end 
